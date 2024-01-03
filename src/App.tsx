3dcf341c44cbc897345a6daab51a7a331f2ad901
Kourosh Eidivandi <Kouroshjs79@gmail.com>
2024-01-03 12:50:44 +0330

    initial design sign up page

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import SigninForm from './_auth/forms/SigninForm'
 import SignupForm from './_auth/forms/SignupForm'
 import RootLayout from './_root/RootLayout'
 import { Home } from './_root/pages'
-import './global.css'
+import './globals.css'
 import { Routes, Route } from 'react-router-dom'
 
 const App = () => {
