d69261be111a6838aabb26cb69817a8b30b06c3e
Kourosh Eidivandi <Kouroshjs79@gmail.com>
2024-01-04 12:08:41 +0330

    connect to appwrite and config apis

--- src/_auth/forms/SignupForm.tsx
+++ src/_auth/forms/SignupForm.tsx
@@ -1,63 +1,127 @@
 import * as z from "zod"
 import { zodResolver } from "@hookform/resolvers/zod"
 import { Button } from "@/components/ui/button"
-import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form"
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form"
 import { Input } from "@/components/ui/input"
 import { useForm } from "react-hook-form"
-
-const formSchema = z.object({
-    username: z.string().min(2).max(50),
-})
-
+import { SignupValidation } from "@/lib/validation"
+import Loader from "@/components/ui/shared/Loader"
+import { Link } from "react-router-dom"
+import { createUserAccount } from "@/lib/appwrite/api"
 
 const SignupForm = () => {
+    const isLoading = false;
     // 1. Define your form.
-    const form = useForm<z.infer<typeof formSchema>>({
-        resolver: zodResolver(formSchema),
+    const form = useForm<z.infer<typeof SignupValidation>>({
+        resolver: zodResolver(SignupValidation),
         defaultValues: {
+            name: "",
             username: "",
+            email: "",
+            password: ""
         },
     })
 
     // 2. Define a submit handler.
-    const onSubmit = (values: z.infer<typeof formSchema>) => {
-        // Do something with the form values.
-        // ✅ This will be type-safe and validated.
-        console.log(values)
+    const onSubmit = async (values: z.infer<typeof SignupValidation>) => {
+        const newUser = await createUserAccount(values);
+
+        console.log(newUser);
     }
     return (
         <Form {...form}>
-            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-                <FormField
-                    control={form.control}
-                    name="username"
-                    render={({ field }) => (
-                        <>
-                            <FormItem>
-                                <FormLabel>Username</FormLabel>
-                                <FormControl>
-                                    <Input placeholder="Input Your Username" {...field} />
-                                </FormControl>
-                                <FormDescription>
-                                    This is your public display name.
-                                </FormDescription>
-                                <FormMessage />
-                            </FormItem>
-                            <FormItem>
-                                <FormLabel>Password</FormLabel>
-                                <FormControl>
-                                    <Input placeholder="Input Your Password" {...field} />
-                                </FormControl>
-                                <FormDescription>
-                                    This is your Password.
-                                </FormDescription>
-                                <FormMessage />
-                            </FormItem>
-                        </>
-                    )}
-                />
-                <Button type="submit">Submit</Button>
-            </form>
+            <div className="sm:w-420 flex-center flex-col">
+                <img src="/assets/images/logo.svg" alt="logo" />
+                <h2 className="h3-bold md:h2-bold pt-5 sm:pt-12">Create a new account</h2>
+                <p className="text-light-3 small-medium md:base-regular">To use Snapgram please enter your details</p>
+
+                <form onSubmit={form.handleSubmit(onSubmit)} className="flex flex-col gap-5 w-full mt-4">
+                    <FormField
+                        control={form.control}
+                        name="name"
+                        render={({ field }) => (
+                            <>
+                                <FormItem>
+                                    <FormLabel>Name</FormLabel>
+                                    <FormControl>
+                                        <Input
+                                            type="text"
+                                            className="shad-input"
+                                            {...field}
+                                        />
+                                    </FormControl>
+                                    <FormMessage className="text-red" />
+                                </FormItem>
+                            </>
+                        )}
+                    />
+                    <FormField
+                        control={form.control}
+                        name="username"
+                        render={({ field }) => (
+                            <>
+                                <FormItem>
+                                    <FormLabel>Username</FormLabel>
+                                    <FormControl>
+                                        <Input
+                                            type="text"
+                                            className="shad-input"
+                                            {...field}
+                                        />
+                                    </FormControl>
+                                    <FormMessage className="text-red" />
+                                </FormItem>
+                            </>
+                        )}
+                    />
+                    <FormField
+                        control={form.control}
+                        name="email"
+                        render={({ field }) => (
+                            <>
+                                <FormItem>
+                                    <FormLabel>Email</FormLabel>
+                                    <FormControl>
+                                        <Input
+                                            type="email"
+                                            className="shad-input"
+                                            {...field}
+                                        />
+                                    </FormControl>
+                                    <FormMessage className="text-red" />
+                                </FormItem>
+                            </>
+                        )}
+                    />
+                    <FormField
+                        control={form.control}
+                        name="password"
+                        render={({ field }) => (
+                            <>
+                                <FormItem>
+                                    <FormLabel>Passowrd</FormLabel>
+                                    <FormControl>
+                                        <Input
+                                            type="password"
+                                            className="shad-input"
+                                            {...field}
+                                        />
+                                    </FormControl>
+                                    <FormMessage className="text-red" />
+                                </FormItem>
+                            </>
+                        )}
+                    />
+                    <Button type="submit" className="shad-button_primary">
+                        {isLoading ? (
+                            <div className="flex-center gap-2"><Loader />Loading ...</div>
+                        ) : "Sign up"}
+                    </Button>
+                    <p className="text-small-regular text-light-2 text-center mt-2">Already have an account?
+                        <Link to='/sign-in' className="text-primary-500 text-small-semibold ml-1">Log In</Link>
+                    </p>
+                </form>
+            </div>
         </Form>
     )
 }
