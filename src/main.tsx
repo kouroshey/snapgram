d3b3ac3e8c1b50fb32492a13816b27e8a5c15873
Kourosh Eidivandi <Kouroshjs79@gmail.com>
2024-01-08 11:08:44 +0330

    handle PostForm functionality

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import ReactDom from "react-dom/client"
 import App from './App'
 import { BrowserRouter } from "react-router-dom"
-import AuthProvider from "./context/AuthContext"
+import { AuthProvider } from "./context/AuthContext"
 import QueryProvider from "./lib/react-query/QueryProvider"
 
 ReactDom.createRoot(document.getElementById('root')!).render(
